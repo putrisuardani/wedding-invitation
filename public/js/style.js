b08216b4c769dcf19819c42c6d4685b5e2959b21
Putri Suardani <putrisuardani94@gmail.com>
2019-10-17 17:20:12 +0800

    update wedding invitation

--- public/js/style.js
+++ public/js/style.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Oct 20, 2019 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
